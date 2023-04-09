574424c5205ecc84f3fe44fb6b2a4b5651881055
RYOTA-F <ry.fj0304@gmail.com>
2023-04-09 21:56:31 +0900

    :sparkles: Add Next.js

---
+++ frontend/pages/index.tsx
@@ -0,0 +1,11 @@
+import { NextPage } from 'next'
+
+const TopPage: NextPage = () => {
+  return (
+    <h1 className="flex items-center justify-center text-[30px] h-[100vh]">
+      TopPage
+    </h1>
+  )
+}
+
+export default TopPage
