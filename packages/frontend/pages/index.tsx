c9f37a419d3571203419ca32d024d4d856c818cd
RYOTA-F <ry.fj0304@gmail.com>
2023-04-12 00:45:44 +0900

    :sparkles: Add Next.js

--- packages/frontend/pages/index.tsx
+++ packages/frontend/pages/index.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div className="flex items-center justify-center h-[100vh]">
-      <h1 className="font-bold text-3xl">Home</h1>
+      <h1 className="font-bold text-4xl">Home</h1>
     </div>
   )
 }
